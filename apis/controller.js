71da018912ac495f7ec07853bc0c5d505f744a8a
Nete <Null>
2024-09-18 08:33:46 -0700

    DB Connection and Fundraisers endpoint

--- apis/controller.js
+++ apis/controller.js
@@ -1,5 +1,14 @@
 const express=require('express')
-
+const db=require('../db/db-details')
+const connection=db.connection()
+connection.connect()
 const route=express.Router()
-
-route.get()
+route.get("/fundraisers", (req, res)=>{
+	connection.query("select * from fundraisers", (err, records,fields)=> {
+		 if (err){
+			 console.error("Error while retrieve the data");
+		 }else{
+			 res.send(records);
+		 }
+	})
+})
--- db/db-details.js
+++ db/db-details.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
+module.exports = {
+  connection:()=>mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'crowdfundraisers_db'
-  });
+  })}
   
