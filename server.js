0de55d800fd0af56a486aad02d19986b8c2ec0d5
Nete <null>
2024-09-22 00:04:50 +0100

    Listenining on a different port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const fundraiser = require("./apis/controller");
 app.use("/api", fundraiser);
-app.listen(5060);
-console.log("Listenin to api calls at port 5060....")
+app.listen(8080);
+console.log("Listenin to api calls at port 8080....")
 
